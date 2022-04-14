afc55a31337ade8e9ec9f18d903c45fb867a5f89
Kyle Jones <kyle@kylejones.dev>
2022-04-14 13:13:18 -0700

    Retrieve deck id

--- main.js
+++ main.js
@@ -1,25 +1,42 @@
 const drawButton = document.getElementById("draw")
 
 const api = 'http://deckofcardsapi.com/api/deck/';
-
-function newDeck(numberDecks) {
-    return new Promise (resolve => {
-            const url = api + `new/shuffle/?deck_count=${numberDecks}`
-        fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-            resolve(data.deck_id);
-        })
-        .catch(err => {
-            console.log(`Error: ${err}`);
-        });
+const deckID = null;
+function fetchThing(url) {
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        return data;
     })
+    .catch(err => {
+        console.log(`Error: ${err}`);
+    });
 }
 
-async function loggy() {
-    console.log('Calling...');
-    let deckID = await newDeck(1);
-    console.log(deckID);
+
+function newDeck(numberDecks = 1) {
+    const url = api + `new/shuffle/?deck_count=${numberDecks}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        deckID = data.deck_id;
+    })
+    .catch(err => {
+        console.log(`Error: ${err}`);
+    });
 }
-loggy();
+
+function drawFromDeck(deckID, numberToDraw) {
+    const url = api + `${deckID}/draw/?count=${numberToDraw}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(err => {
+        console.log(`Error: ${err}`);
+    });
+}
+
+newDeck(1);
